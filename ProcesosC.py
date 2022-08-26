2fd16716f0cad14984f9a6b2c1f28f03394080b6
Victor Manuel Guayara Suarez <61199510+guayara504@users.noreply.github.com>
2022-08-26 15:13:30 -0500

    Update ProcesosC.py

--- ProcesosC.py
+++ ProcesosC.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 26 14:06:37 2022
 
-@author: DIGITADOR10
-"""
 import msvcrt
 import mysql.connector as sql
 import os
@@ -17,9 +12,9 @@ conexion = sql.connect(
 
 inputFile=input("\n\nIngrese las cedulas a consultar separadas por comas: ")
 dias = input("\nPeriodo de tiempo a consultar: ")
+fotos = input("\n¿Tiene fotos?\n1. Si\n2. No\n= ")
 listFile=inputFile.split(",")
 os.system ("cls")
-
 
 for file in listFile:
          
@@ -31,19 +26,20 @@ JOIN z01_radicacion_has_z02_abogado ON z01_radicacion_has_z02_abogado.z01_radica
 AND z01_radicacion_has_z02_abogado.z01_radicacion_z01_radicacion=z04_estado.z01_radicacion_z01_radicacion
 AND z01_radicacion_has_z02_abogado.ciudad=z04_estado.ciudad
 JOIN z02_abogado ON z01_radicacion_has_z02_abogado.z02_abogado_idcedula_z02=z02_abogado.idcedula_z02
-WHERE z02_abogado.privilegio=1 AND z04_estado.fecha_notificacion BETWEEN (CURDATE() - INTERVAL {dias} DAY) AND CURDATE() AND z02_abogado.idcedula_z02 LIKE "%{file}%"
+WHERE z02_abogado.privilegio={fotos} AND z04_estado.fecha_notificacion BETWEEN (CURDATE() - INTERVAL {dias} DAY) AND CURDATE() AND z02_abogado.idcedula_z02 LIKE "%{file}%"
 HAVING COUNT(id_z04_estado)"""
 
 
     print("-"*15)
-    print("Proceso:")
     cursor = conexion.cursor()
     cursor.execute(cedula)
     consultasBD  = [item for item in cursor.fetchall()]
-       
-    print(f"La cedula: {file} Tiene:\nMovimientos: {consultasBD[0][0]}\nProcesos: {consultasBD[0][1]}")
+    if len(consultasBD) == 0:
+        print(f"Cedula: {file}:\n")
+        print("Error!\n-Revise Si las cedulas estan bien digitadas.\n-Revise si el cliente tiene fotos.\n")
+    else:
+        print(f"La cedula: {file} Tiene:\nMovimientos: {consultasBD[0][0]}\nProcesos: {consultasBD[0][1]}")
     print("-"*15)
 
 print("--Presione una tecla para cerrar--")
 msvcrt.getch()
-
